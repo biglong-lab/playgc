a6ce927b73e0d80d73e66b24ac614b4719c22573
biglong <biglong@homi.cc>
2026-02-22 04:21:57 +0800

    chore(auto): 自動存檔 client/src/components/admin-login/index.ts

--- client/src/components/admin-login/index.ts
+++ client/src/components/admin-login/index.ts
@@ -2,5 +2,4 @@
 export { default as FieldCodeBadge } from "./FieldCodeBadge";
 export { default as LoginErrorAlert } from "./LoginErrorAlert";
 export { default as CurrentUserBadge } from "./CurrentUserBadge";
-export { default as PasswordForm } from "./PasswordForm";
 export { default as PendingAuthView } from "./PendingAuthView";
