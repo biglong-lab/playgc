22d984036365452e6716a53de061f0ce32298667
biglong <biglong@homi.cc>
2026-02-22 12:28:07 +0800

    chore(auto): 自動存檔 client/src/pages/AdminStaffPlayers.tsx

--- client/src/pages/AdminStaffPlayers.tsx
+++ client/src/pages/AdminStaffPlayers.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import AdminStaffLayout from "@/components/AdminStaffLayout";
+import UnifiedAdminLayout from "@/components/UnifiedAdminLayout";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import {
