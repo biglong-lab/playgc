bba311a947f62bc24de0944511e680b4ae719f95
biglong <biglong@homi.cc>
2026-03-09 00:21:26 +0800

    chore(auto): 自動存檔 client/src/lib/firebase.ts

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -19,7 +19,8 @@ import {
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN
+    || `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebasestorage.app`,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
