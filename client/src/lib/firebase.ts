4672ef415a2aaf0c75306f41d5f16d8b08d6fb50
biglong <biglong@homi.cc>
2026-03-09 00:51:44 +0800

    chore(auto): 自動存檔 client/src/lib/firebase.ts

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -29,6 +29,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 export const appleProvider = new OAuthProvider('apple.com');
 appleProvider.addScope('email');
 appleProvider.addScope('name');
