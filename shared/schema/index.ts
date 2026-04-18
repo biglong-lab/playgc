32952d6d912c55799d6f2ab5912733fd2030f4c0
biglong <biglong@homi.cc>
2026-04-18 17:30:13 +0800

    chore(auto): 自動存檔 shared/schema/index.ts

--- shared/schema/index.ts
+++ shared/schema/index.ts
@@ -31,3 +31,4 @@ export * from "./platform-features";
 export * from "./platform-billing";
 export * from "./platform-admins";
 export * from "./field-applications";
+export * from "./field-memberships";
