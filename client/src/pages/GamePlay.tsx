4a829a016e8ab74fbcb59d6c3546137db805a5e8
biglong <biglong@homi.cc>
2026-04-18 23:37:12 +0800

    chore(auto): 自動存檔 client/src/pages/GamePlay.tsx

--- client/src/pages/GamePlay.tsx
+++ client/src/pages/GamePlay.tsx
@@ -275,6 +275,7 @@ export default function GamePlay() {
             variables={variables}
             inventory={inventory}
             score={score}
+            visitedLocations={visitedLocations}
           />
         )}
       </main>
