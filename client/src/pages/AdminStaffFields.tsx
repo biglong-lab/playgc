bebab1706955352b2555e10595a2a9e2803ad82c
biglong <biglong@homi.cc>
2026-04-18 15:53:29 +0800

    chore(auto): 自動存檔 client/src/pages/AdminStaffFields.tsx

--- client/src/pages/AdminStaffFields.tsx
+++ client/src/pages/AdminStaffFields.tsx
@@ -358,9 +358,7 @@ export default function AdminStaffFields() {
           </CardHeader>
           <CardContent>
             {isLoading ? (
-              <div className="flex justify-center py-8">
-                <div className="animate-spin w-8 h-8 border-4 border-primary border-t-transparent rounded-full" />
-              </div>
+              <ListSkeleton count={5} />
             ) : fields && fields.length > 0 ? (
               <Table>
                 <TableHeader>
