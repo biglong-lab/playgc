10b836c86321daa20054bad765b9c85c892edbdc
biglong <biglong@homi.cc>
2026-04-22 23:12:39 +0800

    chore(auto): 自動存檔 client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -49,6 +49,7 @@ const FieldAdminLogin = lazy(() => import("@/pages/FieldAdminLogin"));
 const AdminDashboard = lazy(() => import("@/pages/AdminDashboard"));
 const AdminGames = lazy(() => import("@/pages/AdminGames"));
 const AdminLive = lazy(() => import("@/pages/AdminLive"));
+const JoinWalkie = lazy(() => import("@/pages/JoinWalkie"));
 const AdminDevices = lazy(() => import("@/pages/admin-devices"));
 const AdminAnalytics = lazy(() => import("@/pages/AdminAnalytics"));
 const AdminSessions = lazy(() => import("@/pages/AdminSessions"));
