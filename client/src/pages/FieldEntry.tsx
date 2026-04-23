16d4b73c278185e65cc69306e360cc7e0607edfb
biglong <biglong@homi.cc>
2026-04-24 07:12:51 +0800

    chore(auto): 自動存檔 client/src/pages/FieldEntry.tsx

--- client/src/pages/FieldEntry.tsx
+++ client/src/pages/FieldEntry.tsx
@@ -8,7 +8,7 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { Link, useLocation } from "wouter";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import {
