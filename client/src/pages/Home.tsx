06050609d84b3f3e729595f276c08abfb017ad6f
biglong <biglong@homi.cc>
2026-04-24 05:44:16 +0800

    chore(auto): 自動存檔 client/src/pages/Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -21,6 +21,7 @@ import { useToast } from "@/hooks/use-toast";
 import { useCurrentField } from "@/providers/FieldThemeProvider";
 import { useFieldLink } from "@/hooks/useFieldLink";
 import OptimizedImage from "@/components/shared/OptimizedImage";
+import GenericCoverFallback from "@/components/shared/GenericCoverFallback";
 
 interface UserGameStatus {
   gameId: string;
