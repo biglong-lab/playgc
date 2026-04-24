519b05f677ff22e39fad41743b8fee25448ed085
biglong <biglong@homi.cc>
2026-04-24 15:27:44 +0800

    chore(auto): 自動存檔 client/src/pages/AdminStaffPlayers.tsx

--- client/src/pages/AdminStaffPlayers.tsx
+++ client/src/pages/AdminStaffPlayers.tsx
@@ -11,6 +11,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Switch } from "@/components/ui/switch";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Dialog,
   DialogContent,
