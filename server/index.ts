0fe67fc50b563a8056e793f8ce6107a9edb15c17
biglong <biglong@homi.cc>
2026-04-25 23:37:23 +0800

    chore(auto): 自動存檔 server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -348,6 +348,7 @@ app.use((req, res, next) => {
         startAchievementScheduler();
         startLifecycleScheduler();
         startMonthlyResetScheduler();
+        startRewardRetryWorker();
       }
     },
   );
