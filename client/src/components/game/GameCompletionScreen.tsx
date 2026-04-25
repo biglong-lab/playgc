26c250c155220f6d5df4187fcc1b7580e0da15b6
biglong <biglong@homi.cc>
2026-04-26 04:12:19 +0800

    chore(auto): 自動存檔 client/src/components/game/GameCompletionScreen.tsx

--- client/src/components/game/GameCompletionScreen.tsx
+++ client/src/components/game/GameCompletionScreen.tsx
@@ -10,6 +10,7 @@ import { useToast } from "@/hooks/use-toast";
 import { useCurrentField } from "@/providers/FieldThemeProvider";
 import { useFieldLink } from "@/hooks/useFieldLink";
 import { apiRequest } from "@/lib/queryClient";
+import { savePhotoToAlbum, getSaveToastMessage } from "@/lib/photo-save";
 import { useQuery } from "@tanstack/react-query";
 import {
   Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription,
