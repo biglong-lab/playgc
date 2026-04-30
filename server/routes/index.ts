dbb6604fd56ec409dbd30f924610499bcee59105
biglong <biglong@homi.cc>
2026-05-01 01:09:45 +0800

    chore(auto): 自動存檔 server/routes/index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -86,6 +86,7 @@ export async function registerRoutes(
   registerAdminRoleRoutes(app);
   registerAdminGameRoutes(app);
   registerVariantPoolRoutes(app);
+  registerExemplarRoutes(app);
   registerAdminContentRoutes(app);
   registerPlayerGameRoutes(app);
   registerDeviceRoutes(app, ctx);
