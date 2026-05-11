dc752bba1a5bf1fd0ad4ce7a499c58e87d5f3629
biglong <biglong@homi.cc>
2026-05-12 01:57:18 +0800

    chore(auto): 自動存檔 client/src/components/game/multi/VoteTeamPage.tsx

--- client/src/components/game/multi/VoteTeamPage.tsx
+++ client/src/components/game/multi/VoteTeamPage.tsx
@@ -50,6 +50,8 @@ export default function VoteTeamPage({
 
   // 取得當前玩家的隊伍
   const {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // (telemetry 在下面用 myTeam.id)
     data: myTeam,
     isLoading: teamLoading,
     isError: teamError,
